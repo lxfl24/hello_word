76bb008cdf378bbe43ea7f6c90888c6a3c5fb52f
lxfl24 <38977859+lxfl24@users.noreply.github.com>
2019-05-06 09:41:32 +0800

    Add files via upload

---
+++ config_device.h
@@ -0,0 +1,22 @@
+#ifndef __CONFIG_DEVICE_H
+#define __CONFIG_DEVICE_H
+
+
+
+#include "dev_clock.h"
+#include "dev_gpio.h"
+#include "dev_uart.h"
+#include "dev_systick.h"
+#include "dev_timer.h"
+#include "dev_wdog.h"
+#include "dev_ADC.h"
+#include "dev_msc.h"
+#include "dev_DMA.h"
+#include "dev_RTC.h"
+
+
+#include "YB_Wave.h"
+#include "YB_Connect.h"
+#include "YB_ADC0_Data.h"
+
+#endif
